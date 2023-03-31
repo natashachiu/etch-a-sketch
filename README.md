c38dc25e2fffb7730b859647d662e2b9763add1b
Natasha Chiu <chiuchiu07@gmail.com>
2023-03-31 19:00:00 +0800

    grid size and hover functionality

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# etch-a-sketch
+# Etch-a-sketch
 
-References:
- - [Removing all child nodes of an element](https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/)
+## Preview: 
+- [Live version] (https://natashachiu.github.io/etch-a-sketch/)
 
- 
+## Features:
+- Classic mode: draw with grey color.
+- Rainbow mode: draw with changing random color.
+- Clear: cleans up the canvas.
+- Grid size: change the grid size up between 2x2 and 100x100.
+
+## References:
+ - Removing all child nodes of an element 
+    https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+
+ - Generate RGB colors at random
+    https://code.tutsplus.com/tutorials/how-to-code-a-random-color-generator-in-javascript--cms-39861
+
+ - Toggle button that switches upon click
+    https://linuxhint.com/toggle-button-javascript/#:~:text=A%20tgl()%20method%20is,be%20toggled%20to%20%E2%80%9CON%E2%80%9D.
--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const size = 50;
+const size = 30;
 
 const container = document.querySelector("#container-div");
 
+// ------------- draw on mousedown property ---------------------
+let is_drawing = false;
+container.addEventListener('mousedown', () =>  is_drawing = true);
+container.addEventListener('mouseup', () =>  is_drawing = false);
 
-function removeAllChildNodes(parent) {
-    while(parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+
+// ---------------------- draw mode -----------------------------
+let toggle_rainbow = false;
+
+const mode = document.querySelector('.mode');
+mode.addEventListener('click', toggleMode);
+
+function toggleMode(e) {
+    if (e.target.textContent =="Classic mode") {
+        e.target.textContent = "Rainbow mode";
+        toggle_rainbow = true;
+    } else {
+        e.target.textContent = "Classic mode";
+        toggle_rainbow = false;
     }
 }
 
-// create divs that acts as grid
+// -------------------------- grid ----------------------------------
+
+
 function makeGrid(size){
     removeAllChildNodes(container);
 
@@ -20,33 +37,80 @@ function makeGrid(size){
             const col = document.createElement('div');
             col.classList.add('pixel');
             row.appendChild(col);
+            col.addEventListener('mousedown', addColor);
+            col.addEventListener('mouseover', addColor);
         }
         container.appendChild(row);
     }    
 }
-makeGrid(size);
+
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// --------------------------- color -----------------------------------
 
 // changes color of pixel
 function addColor(e) {
-    e.target.style.background = 'grey';
-}
+    if (e.type === 'mouseover' && !is_drawing) return;
 
-// allows mouse over to color in pixels
-const pixels = document.querySelectorAll('.pixel');
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', addColor)
-});
-
-// prompts user to enter a size for the grid
-function promptSize() {
-    let grid_size = prompt("Enter grid size: ");
-    if (grid_size > 100) {
-        alert("Grid size too large");
-        return;
+    if (toggle_rainbow) {
+        let [r,g,b] =randomRgbColor();
+        e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+    }else {
+        e.target.style.background = 'slategrey';
     }
-    makeGrid(grid_size);
 }
 
-// prompt button event
-const prompt_btn = document.querySelector('.prompt-size');
-prompt_btn.addEventListener('click', promptSize);
+// create random color
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+
+// ----------------------- grid size slider ------------------------------
+
+const range_slider = document.querySelector('#range-slider');
+const output = document.querySelector('.grid-size');
+
+range_slider.addEventListener('input', changeGridSize);
+
+function changeGridSize(e) {
+    output.textContent = `Grid size: ${e.target.value} x ${e.target.value}`;
+    makeGrid(e.target.value);
+}
+
+
+
+// ---------------------------- clear -----------------------------------
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', clearAll);
+// clear the grid
+function clearAll() {
+    makeGrid(size);
+}
+
+makeGrid(size);
+
+
+// ------------------- button click animation -----------------------------
+let button_click = document.querySelectorAll('button');
+
+button_click.forEach(button => {
+
+    button.addEventListener('mousedown', function(e) {
+        e.target.style.background = 'peru';
+    })
+    button.addEventListener('mouseup', function(e) {
+        e.target.style.background = 'goldenrod';
+    })
+})
